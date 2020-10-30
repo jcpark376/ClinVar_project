07cc8ab545feb4ea10637fb42f81aef1938589e8
Jay Park <15186861+jcpark376@users.noreply.github.com>
2020-10-29 23:00:34 -0400

    Reorg and cleanup for code review (except README)

--- evaluations/stream.py
+++ evaluations/stream.py
@@ -1,3 +1,8 @@
+'''
+Code for Streamlit that allows users to alter the features.
+Will output chance of DNA variation being benign vs pathogenic
+'''
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,12 +19,14 @@ st.write('''
 '''
 )
 
+# Sliders and drop-down menus
 alfreq = st.slider('% of Population Carrying Allele', float(0), float(1), 0.5) / 100
 cadd = st.slider('CADD(RAW) Score', float(-6), float(45), float(20))
 blosum = st.slider('BLOSUM62 Score', int(-4), int(11), int(3))
 loftool = st.slider('LoFtool Score', float(0), float(1), float(0.5))
 polyphen = st.selectbox('PolyPhen Category', ['Benign','Unknown','Possibly damaging','Probably damaging'])
 sift = st.selectbox('SIFT Category', ['Tolerated','Likely Tolerated','Unknown','Likely Deleterious','Deleterious'])
+# Initilize the dictionary for the one-hot-encoded columns
 poly_sift_dict = {
 	'PolyPhen_benign': 0, 
 	'PolyPhen_possibly_damaging': 0,
@@ -32,6 +39,7 @@ poly_sift_dict = {
     'SIFT_tolerated_low_confidence': 0
 }
 
+# If user selects PolyPhen or SIFT values, update the dictionary
 if polyphen == 'Benign':
 	poly_sift_dict['PolyPhen_benign'] = 1
 if polyphen == 'Unknown':
@@ -40,6 +48,8 @@ if polyphen == 'Possibly damaging':
 	poly_sift_dict['Polyphen_possibly_damaging'] = 1
 if polyphen == 'Probably damaging':
 	poly_sift_dict['Polyphen_probably_damaging'] = 1
+
+
 if sift == 'Tolerated':
 	poly_sift_dict['SIFT_tolerated'] = 1
 if sift == 'Likley Tolerated':
@@ -51,6 +61,7 @@ if sift == 'Likley Deleterious':
 if sift == 'Deleterious':
 	poly_sift_dict['SIFT_deleterious'] = 1
 
+# Single-row dataframe representing user input of parameters
 input_data = pd.DataFrame({
 	'AF_ESP': [alfreq], 
 	'AF_EXAC': [alfreq], 
@@ -70,18 +81,18 @@ input_data = pd.DataFrame({
 	})
 
 
+# Predict the probablities that DNA variant will be benign vs pathogenic. 
+# Divide them by the sum
 pred_be = xg_clas.predict_proba(input_data)[0][0] * 100
 pred_path = xg_clas.predict_proba(input_data)[0][2] * 100
-# pred_unc = xg_clas.predict_proba(input_data)[0][1] * 100
+
 benign = pred_be / (pred_be + pred_path) * 100
 patho = pred_path / (pred_be + pred_path) * 100
 
 st.write(
 f'Chance that the Variant is of Benign Consequence: {benign:.1f}%'
 )
-# st.write(
-# f'Chance that the Variant is of Uncertain Consequence: {pred_unc:.1f}%'
-# )
+
 st.write(
 f'Chance that the Variant is of Pathogenic Consequence {patho:.1f}%'
 )
